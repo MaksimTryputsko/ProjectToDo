720738417a4accdc01085a409fd81cda65645606
Maksim <>
2023-06-09 18:55:28 +0300

    secondCommit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './main.scss';
+// import html from './template.html';
+
+function component(text) {
+    const element = document.createElement('h1');
+    element.textContent = text;
+    return element;
+  }
+  document.body.prepend(component('Проект собран на Webpack'));
